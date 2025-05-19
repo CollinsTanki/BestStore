60b43028b5def4d575631983001ac2ec2bfc5f71
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2025-05-19 11:25:12 +0300

    Add project files.

---
+++ BestStoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BestStoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
